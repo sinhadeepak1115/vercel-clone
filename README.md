5646bc95f9f260ebb10168872203d532ac1fa8e5
Deepak Kumar Sinha <94794077+sinhadeepak1115@users.noreply.github.com>
2024-02-17 03:31:27 +0530

    making of the script.

---
+++ README.md
@@ -0,0 +1,17 @@
+<h1 align="center">Welcome to vercel-clone 👋</h1>
+<p>
+</p>
+
+> Making a vercel clone.
+
+## Author
+
+👤 **Deepak Kr Sinha**
+
+
+## Show your support
+
+Give a ⭐️ if this project helped you!
+
+***
+_This README was generated with ❤️ by [readme-md-generator](https://github.com/kefranabg/readme-md-generator)_
---
+++ build-server/script.js
@@ -0,0 +1,21 @@
+const { exec } = require('child_process')
+const path = require('path')
+
+async function init() {
+  console.log('Executing script.js')
+  const outDirPath = path.join(__dirname, 'output')
+
+  const p = exec(`cd ${outDirPath} && npm install && npm run build`)
+
+  p.stdout.on('data', function(data) {
+    console.log(data.toString())
+  })
+
+  p.stdout.on('error', function(data) {
+    console.log('Error', data.toString())
+  })
+
+  p.on('close', function() {
+    console.log('Build Complete')
+  })
+}
