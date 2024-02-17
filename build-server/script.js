73206afa7830ff3df2114d84e189e7a46cd846d2
Deepak Kumar Sinha <94794077+sinhadeepak1115@users.noreply.github.com>
2024-02-17 15:43:13 +0530

    checkpoint

--- build-server/script.js
+++ build-server/script.js
@@ -1,5 +1,6 @@
 const { exec } = require('child_process')
 const path = require('path')
+const fs = require('fs')
 
 async function init() {
   console.log('Executing script.js')
@@ -15,7 +16,14 @@ async function init() {
     console.log('Error', data.toString())
   })
 
-  p.on('close', function() {
+  p.on('close', async function() {
     console.log('Build Complete')
+    const distFolderPath = path.join(__dirname, 'output', 'dist')
+    const distFolderContents = fs.readdirSync(distFolderPath, {
+      recursive: true
+    })
+    for (const filePath of distFolderContents) {
+      if (fs.lstatSync(filePath).isDirectory()) continue;
+    }
   })
 }
